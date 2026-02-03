9d5cd37cde3a3933483fa26169b52f1c21c1b471
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-02-03 14:53:25 +0100

    feat: add member service updating and deleting flow

--- src/features/family/family.service.ts
+++ src/features/family/family.service.ts
@@ -7,3 +7,37 @@ export const getMembers = async (): Promise<Member[]> =>
 
 export const saveMembers = async (members: Member[]) =>
   localStorage.setItem(KEY, JSON.stringify(members));
+
+export const updateMemberStorage = async (data: Member) => {
+  const stored = localStorage.getItem(KEY);
+  if (!stored) return [];
+
+  const members: Member[] = JSON.parse(stored);
+
+  const newMembers = members.map((member) =>
+    member.id === data.id ? { ...member, ...data } : member
+  );
+
+  localStorage.setItem(KEY, JSON.stringify(newMembers));
+
+  return newMembers;
+}
+
+export const deleteMemberStorage = (id: string) => {
+  const stored = localStorage.getItem(KEY);
+  if (!stored) return [];
+
+  let members: Member[] = JSON.parse(stored);
+
+  const hasChildren = members.some((m) => m.parentId === id);
+  if (hasChildren) {
+    throw new Error("Cannot delete member with children");
+  }
+
+  const newMembers = members.filter((member) => member.id !== id);
+  localStorage.setItem(KEY, JSON.stringify(newMembers));
+
+  return newMembers;
+};
+
+
